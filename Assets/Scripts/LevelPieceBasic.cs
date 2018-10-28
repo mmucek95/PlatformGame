950fef6306fe2764ac64585b3c1985d03f03ea06
unknown <m_mucek@wp.pl>
2018-10-28 13:57:46 +0100

    Level generator. No camera following.

---
+++ Assets/Scripts/LevelPieceBasic.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPieceBasic : MonoBehaviour {
+
+    public Transform exitPoint;
+    public Transform startPoint;
+}
